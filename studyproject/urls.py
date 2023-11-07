7f23893ccc08290431f45c7a632010c4568c53cf
Darshan <darshanpatel2923@gmail.com>
2023-11-07 20:15:47 +0530

    started with server

--- studyproject/urls.py
+++ studyproject/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Home page')
+    return HttpResponse("Home page")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
