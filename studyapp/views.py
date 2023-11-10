3599ffbf93acdef3c8573f39eb960d35f954ddd6
Darshan <darshanpatel2923@gmail.com>
2023-11-10 20:12:25 +0530

    Added update functionality to the project

--- studyapp/views.py
+++ studyapp/views.py
@@ -4,11 +4,11 @@ from .models import Room
 from .forms import RoomForm
 # Create your views here.
 
-rooms=[
-    {"id":1,'name':"Let's learn python"},
-    {"id":2,'name':"Design with me"},
-    {"id":3,'name':"Frontend development"},
-]
+# rooms=[
+#     {"id":1,'name':"Let's learn python"},
+#     {"id":2,'name':"Design with me"},
+#     {"id":3,'name':"Frontend development"},
+# ]
 
 def home(request):
     rooms=Room.objects.all()
@@ -30,6 +30,19 @@ def createRoom(request):
       print(request.POST)
       # request.post.get('name')
       form=RoomForm(request.POST)
+      if form.is_valid():
+         form.save()
+         return redirect('home')
+   context={'form':form}
+   return render(request,'studyapp/room_form.html',context)
+
+
+def updateRoom(request,pk):
+   room=Room.objects.get(id=pk)
+   form=RoomForm(instance=room)
+
+   if request.method=='POST':
+      form=RoomForm(request.POST,instance=room)
       if form.is_valid():
          form.save()
          return redirect('home')
