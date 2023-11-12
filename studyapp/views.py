9b3c90607593c84652e28bb01dec98e1c9c1e658
Darshan <darshanpatel2923@gmail.com>
2023-11-12 17:39:44 +0530

    All functionality of login and logout done

--- studyapp/views.py
+++ studyapp/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from .models import Room,Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 # rooms=[
@@ -21,9 +23,21 @@ def loginPage(request):
       try:
          user=User.objects.get(username=username)
       except:
-         print("User already exists!!!")
+         messages.error(request,'User does not exists!!!')
+
+      user=authenticate(request,username=username,password=password) #returns user object or None if user do not exists
+
+      if user is not None:
+         login(request,user)
+         return redirect('home')
+      else:
+         messages.error("Invalid credentials")
    context={}
    return render(request,'studyapp/login_register.html',context)
+
+def logoutUser(request):
+   logout(request)
+   return redirect('home')
 
 def home(request):
     q=request.GET.get('q') if request.GET.get('q')!=None else ''
