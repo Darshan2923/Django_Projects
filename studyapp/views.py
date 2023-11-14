47019b8ee0b36d03d3382ff66cc3502ccb622580
Darshan <darshanpatel2923@gmail.com>
2023-11-14 20:34:33 +0530

    Little customizations onto the host shit

--- studyapp/views.py
+++ studyapp/views.py
@@ -109,7 +109,9 @@ def createRoom(request):
       # request.post.get('name')
       form=RoomForm(request.POST)
       if form.is_valid():
-         form.save()
+         room=form.save(commit=False)
+         room.host=request.user
+         room.save()
          return redirect('home')
    context={'form':form}
    return render(request,'studyapp/room_form.html',context)
