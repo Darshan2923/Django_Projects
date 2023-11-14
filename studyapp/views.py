a1dfc8e9de2583db54bd8415afe6949ae629b154
Darshan <darshanpatel2923@gmail.com>
2023-11-14 20:14:35 +0530

    MAking user profile dynamic in the project

--- studyapp/views.py
+++ studyapp/views.py
@@ -95,7 +95,10 @@ def room(request,pk):
 
 def userProfile(request,pk):
    user=User.objects.get(id=pk)
-   context={'user':user}
+   rooms=user.room_set.all()
+   room_message=user.message_set.all()
+   topics=Topic.objects.all()
+   context={'user':user,'rooms':rooms,'room_message':room_message,'topics':topics}
    return render(request,'studyapp/userProfile.html',context)
 
 @login_required(login_url='login')
