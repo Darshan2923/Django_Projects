02028b02f1fe82248fde2c952c54782d92f55efc
Darshan <darshanpatel2923@gmail.com>
2023-11-14 10:42:53 +0530

    Officially completed the message functionality

--- studyapp/views.py
+++ studyapp/views.py
@@ -132,3 +132,15 @@ def deleteRoom(request,pk):
       room.delete()
       return redirect('home')
    return render(request,'studyapp/delete.html',{'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+   message=Message.objects.get(id=pk)
+
+   if request.user!=message.user:
+      return HttpResponse("Your'e not allowed to do this!!!")
+   
+   if request.method=='POST':
+      message.delete()
+      return redirect('home')
+   return render(request,'studyapp/delete.html',{'obj':message})
