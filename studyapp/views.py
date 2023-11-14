041f47ef67825dcb0eb58bd2ff6c31a02c204b01
Darshan <darshanpatel2923@gmail.com>
2023-11-14 16:45:06 +0530

    Segregated in different components  (reusability)

--- studyapp/views.py
+++ studyapp/views.py
@@ -69,7 +69,7 @@ def home(request):
     rooms=Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
     topic=Topic.objects.all()
     room_count=rooms.count()
-    room_messages=Message.objects.all()
+    room_messages=Message.objects.filter(room__topic__name__icontains=q)
     context={"rooms":rooms,"topic":topic,"room_count":room_count,'room_messages':room_messages}
     return render(request,'studyapp/home.html',context)
 
