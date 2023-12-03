bd4ef1ca27198a25963a186c2e1f25aeca9f5ec6
Darshan <darshanpatel2923@gmail.com>
2023-12-03 15:37:09 +0530

    Added a little templates and static files

--- Django_ecommerce/ecomapp/views.py
+++ Django_ecommerce/ecomapp/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the Home page")
-    
+    context={}
+    return render(request,'ecomapp/index.html',context)
 
-def books(request):
-    return HttpResponse("This is the Books page")
+def cart(request):
+    context={}
+    return render(request,'ecomapp/cart.html',context)
+
+def store(request):
+    context={}
+    return render(request,'ecomapp/store.html',context)
+
+def checkout(request):
+    context={}
+    return render(request,'ecomapp/checkout.html',context)
     
     
