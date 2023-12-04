041f09c93d44ef69fc4c88c8707cca71e39bcf5e
Darshan <darshanpatel2923@gmail.com>
2023-12-04 21:31:25 +0530

    added views to product db model & render template

--- Django_ecommerce/ecomapp/views.py
+++ Django_ecommerce/ecomapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -10,7 +11,8 @@ def cart(request):
     return render(request,'ecomapp/cart.html',context)
 
 def store(request):
-    context={}
+    products=Product.objects.all()
+    context={'products':products}
     return render(request,'ecomapp/store.html',context)
 
 def checkout(request):
