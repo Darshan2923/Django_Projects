8d5d3468cc82e43bc0d6126aaabee22765b5258d
Darshan <darshanpatel2923@gmail.com>
2023-12-04 20:44:43 +0530

    Added models for ordering the item and products

--- Django_ecommerce/ecomapp/models.py
+++ Django_ecommerce/ecomapp/models.py
@@ -9,3 +9,34 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    name=models.CharField(max_length=200)
+    price=models.FloatField()
+    digital=models.BooleanField(default=False,null=True,blank=True)
+    # image
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
+    date_ordered=models.DateTimeField(auto_now_add=True)
+    complete=models.BooleanField(default=False)
+    transaction_id=models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return str(self.id)
+    
+#Note Foreign Key is many to one relationaship. Lol!!
+
+class OrderItem(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
+    order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
+    quantity=models.IntegerField(default=0,null=True,blank=True)
+    date_added=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.product)
+    
+    
