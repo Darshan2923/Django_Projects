be02bdade2ffae7633907808f900184d6d608faf
Darshan <darshanpatel2923@gmail.com>
2023-12-08 18:21:43 +0530

    Added a lot in products section of the project

--- Django_ecommerce/ecomproj/settings.py
+++ Django_ecommerce/ecomproj/settings.py
@@ -122,6 +122,10 @@ STATICFILES_DIRS=[
     BASE_DIR/'static'
 ]
 
+MEDIA_ROOT=BASE_DIR/'static/img'
+
+MEDIA_URL='/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
