1bc7b24a7b226e5b9a0943b8d7fe652140f2e0b9
Darshan <darshanpatel2923@gmail.com>
2023-12-09 22:41:47 +0530

    Created add to cart eventlistener and console log user login

---
+++ Django_ecommerce/ecomapp/static/js/cart.js
@@ -0,0 +1,19 @@
+// console.log("Hello Django!!")
+
+let updateBtns = document.getElementsByClassName('update-cart')
+
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId: ', productId, 'Action: ', action)
+
+        console.log('User: ', user);
+        if (user == 'AnonymousUser') {
+            console.log('User is not authenticated')
+        }
+        else {
+            console.log('User authenticated')
+        }
+    })
+}
