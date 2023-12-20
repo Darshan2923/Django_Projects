ea2bf3043a30affa3f630e4af631d0102048ab93
Darshan <darshanpatel2923@gmail.com>
2023-12-20 16:16:29 +0530

    Resolved RelatedObjectDoesNotExist error

--- Django_ecommerce/ecomapp/views.py
+++ Django_ecommerce/ecomapp/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render,HttpResponse
-from .models import *
+from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import datetime
+from .models import * 
 
-# Create your views here.
-def index(request):
-    context={}
-    return render(request,'ecomapp/index.html',context)
+def store(request):
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+		cartItems = order['get_cart_items']
+
+	products = Product.objects.all()
+	context = {'products':products, 'cartItems':cartItems}
+	return render(request, 'ecomapp/store.html', context)
 
 def cart(request):
 
@@ -15,101 +27,66 @@ def cart(request):
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
-		cartItems=order.get_cart_items
+		cartItems = order.get_cart_items
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
-		cartItems=order['get_cart_items']
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+		cartItems = order['get_cart_items']
 
-	context = {'items':items, 'order':order,'cartItems':cartItems}
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'ecomapp/cart.html', context)
 
-def store(request):
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-		cartItems=order.get_cart_items
-	else:
-		#Create empty cart for now for non-logged in user
-		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
-		cartItems=order['get_cart_items']
-	
-	products=Product.objects.all()
-	context={'products':products,'order':order,'items':items,'cartItems':cartItems}
-	return render(request,'ecomapp/store.html',context)
-
-
 def checkout(request):
-
 	if request.user.is_authenticated:
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
-		cartItems=order.get_cart_items
+		cartItems = order.get_cart_items
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
-		cartItems=order['get_cart_items']
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+		cartItems = order['get_cart_items']
 
-	context = {'items':items, 'order':order,'cartItems':cartItems}
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'ecomapp/checkout.html', context)
 
-
-
 def updateItem(request):
-	data=json.loads(request.body)
-	productId=data['productId']
-	action=data['action']
-	print('Action: ',action)
-	print('productId: ',productId)
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
 
-	customer=request.user.customer
-	product=Product.objects.get(id=productId)
-	order,created=Order.objects.get_or_create(customer=customer,complete=False)
-	orderItem,created=OrderItem.objects.get_or_create(order=order,product=product)
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
-	if action=='add':
-		orderItem.quantity=(orderItem.quantity+1)
-	if action=='remove':
-		orderItem.quantity=(orderItem.quantity-1)
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
 
 	orderItem.save()
 
-	if orderItem.quantity<=0:
+	if orderItem.quantity <= 0:
 		orderItem.delete()
-	return JsonResponse('Item was added',safe=False)
-    
+
+	return JsonResponse('Item was added', safe=False)
+
 def processOrder(request):
-	print('Data: ',request.body)
-	transaction_id=datetime.datetime().timestamp()
-	data=json.loads(request.body)
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
 
 	if request.user.is_authenticated:
-		customer=request.user.customer
-		order,created=Order.objects.get_or_create(customer=customer,complete=False)
-		total=float(data['form']['total'])
-		order.transaction_id=transaction_id
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+	else:
+		print('User is not logged in')
 
-		if total==order.get_cart_total:
-			order.complete=True
-		order.save()
-
-		if order.shipping==True:
-			ShippingAddress.objects.create(
-            customer=customer,
-            order=order,
-            adress=data['shipping']['address'],
-            city=data['shipping']['city'],
-            state=data['shipping']['state'],
-            zipcode=data['shipping']['zipcode'],
-        )
-
-
-
-
-	return JsonResponse('Payment submitted...',safe=False)
-
+	return JsonResponse('Payment submitted..', safe=False)
