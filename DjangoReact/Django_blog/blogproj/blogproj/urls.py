85af1e53e982f6f8ba53eb35c3cc19824ee346e0
Darshan <darshanpatel2923@gmail.com>
2023-12-28 12:31:38 +0530

    Starting with blog app instead of django react

--- DjangoReact/Django_blog/blogproj/blogproj/urls.py
+++ DjangoReact/Django_blog/blogproj/blogproj/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DjangoReact project.
+URL configuration for blogproj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
