aa1dd5d86f4d81976953b59eb8d70a8374b6166a
Darshan <darshanpatel2923@gmail.com>
2023-12-28 15:18:56 +0530

    Added authentication(registration) form

--- Django_blog/Django_blog/blogapp/views.py
+++ Django_blog/Django_blog/blogapp/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
+
+
+def index(request):
+    context={}
+    return render(request,'blogapp/index.html',context)
+
+def register_page(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        user_obj=User.objects.filter(username=username)
+        if user_obj.exists():
+          messages.error("User already exists!!")
+          return redirect('/login/')
+        user = User.objects.create_user(username, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        # user_obj=User.objects.user(username=username)
+        # user_obj.set_password(password)
+        return redirect('/login/')
+
+    return render(request,'blogapp/register_page.html')
+
+def login_page(request):
+    if request.method=='POST':
+      try:
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user_obj=User.objects.filter(username=username)
+        if not user_obj.exists():
+            messages.error(request,"Username does not exists!!!")
+            return redirect('/login/')
+        user_obj=authenticate(username,password)
+        if user_obj.is_authenticated():
+           login(request,user_obj)
+           messages.success(request,"User logged in successfully!!")
+           return redirect('/')
+        messages.error("Passwords Doesnt match!!")
+
+      except Exception as e:
+         messages.error(request,"Something went wrong!")
+         return redirect('/login/')
+    return render(request,'blogapp/login_page.html')
+ 
+
+def logout_user(request):
+    logout(request)
+    return redirect('/login/')
