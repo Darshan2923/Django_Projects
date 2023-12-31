f17e611a7ff0ad942cd64ee559a29228d7ded881
Darshan <darshanpatel2923@gmail.com>
2023-12-31 17:08:06 +0530

    Started with a simpler chat app

--- Dwitter_social_app/Digisnap/Digisnap/asgi.py
+++ Dwitter_social_app/Digisnap/Digisnap/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dwitter project.
+ASGI config for Digisnap project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dwitter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Digisnap.settings')
 
 application = get_asgi_application()
